c901fc865039073122657c8974d59966289e9a8e
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2024-02-23 12:03:37 +1300

    Update 03_check_quality_of_cleaned_data.sh

--- 00_scripts/03_check_quality_of_cleaned_data.sh
+++ 00_scripts/03_check_quality_of_cleaned_data.sh
@@ -5,32 +5,14 @@
 
 # Correct tool citation : Andrews, S. (2010). FastQC: a quality control tool for high throughput sequence data.
 
-## WORKING_DIRECTORY=/scratch_vol1/fungi/Mayotte_microorganism_colonisation/03_cleaned_data/Original_reads_TUFA
-## OUTPUT=/scratch_vol1/fungi/Mayotte_microorganism_colonisation/04_quality_check/Original_reads_TUFA
-## 
-## # Make the directory (mkdir) only if not existe already(-p)
-## mkdir -p $OUTPUT
-## 
-## eval "$(conda shell.bash hook)"
-## conda activate fastqc
-## 
-## cd $WORKING_DIRECTORY
-## 
-## for FILE in $(ls $WORKING_DIRECTORY/*.fastq.gz)
-## do
-##       fastqc $FILE -o $OUTPUT
-## done ;
-## 
-## conda deactivate fastqc
-## conda activate multiqc
-## 
-## # Run multiqc for quality summary
-## 
-## multiqc $OUTPUT
-## 
-## 
-WORKING_DIRECTORY=/scratch_vol1/fungi/Mayotte_microorganism_colonisation/03_cleaned_data/Original_reads_16S_ITS_18S
-OUTPUT=/scratch_vol1/fungi/Mayotte_microorganism_colonisation/04_quality_check/Original_reads_16S_ITS_18S
+######################################################################################################################################################
+######################################################################################################################################################
+###### 16S ######
+######################################################################################################################################################
+######################################################################################################################################################
+
+WORKING_DIRECTORY=/home/fungi/Mayotte_microorganism_colonisation/03_cleaned_data/16S
+OUTPUT=/home/fungi/Mayotte_microorganism_colonisation/04_quality_check/16S
 
 # Make the directory (mkdir) only if not existe already(-p)
 mkdir -p $OUTPUT
@@ -45,9 +27,101 @@ do
       fastqc $FILE -o $OUTPUT
 done ;
 
+
 conda deactivate fastqc
 conda activate multiqc
 
 # Run multiqc for quality summary
 
 multiqc $OUTPUT
+
+######################################################################################################################################################
+######################################################################################################################################################
+###### ITS ######
+######################################################################################################################################################
+######################################################################################################################################################
+
+WORKING_DIRECTORY=/home/fungi/Mayotte_microorganism_colonisation/03_cleaned_data/ITS
+OUTPUT=/home/fungi/Mayotte_microorganism_colonisation/04_quality_check/ITS
+
+# Make the directory (mkdir) only if not existe already(-p)
+mkdir -p $OUTPUT
+
+eval "$(conda shell.bash hook)"
+conda activate fastqc
+
+cd $WORKING_DIRECTORY
+
+for FILE in $(ls $WORKING_DIRECTORY/*.fastq.gz)
+do
+      fastqc $FILE -o $OUTPUT
+done ;
+
+
+conda deactivate fastqc
+conda activate multiqc
+
+# Run multiqc for quality summary
+
+multiqc $OUTPUT
+
+######################################################################################################################################################
+######################################################################################################################################################
+###### TUFA ######
+######################################################################################################################################################
+######################################################################################################################################################
+
+WORKING_DIRECTORY=/home/fungi/Mayotte_microorganism_colonisation/03_cleaned_data/TUFA
+OUTPUT=/home/fungi/Mayotte_microorganism_colonisation/04_quality_check/TUFA
+
+# Make the directory (mkdir) only if not existe already(-p)
+mkdir -p $OUTPUT
+
+eval "$(conda shell.bash hook)"
+conda activate fastqc
+
+cd $WORKING_DIRECTORY
+
+for FILE in $(ls $WORKING_DIRECTORY/*.fastq.gz)
+do
+      fastqc $FILE -o $OUTPUT
+done ;
+
+
+conda deactivate fastqc
+conda activate multiqc
+
+# Run multiqc for quality summary
+
+multiqc $OUTPUT
+
+######################################################################################################################################################
+######################################################################################################################################################
+###### 18S ######
+######################################################################################################################################################
+######################################################################################################################################################
+
+WORKING_DIRECTORY=/home/fungi/Mayotte_microorganism_colonisation/03_cleaned_data/18S
+OUTPUT=/home/fungi/Mayotte_microorganism_colonisation/04_quality_check/18S
+
+# Make the directory (mkdir) only if not existe already(-p)
+mkdir -p $OUTPUT
+
+eval "$(conda shell.bash hook)"
+conda activate fastqc
+
+cd $WORKING_DIRECTORY
+
+for FILE in $(ls $WORKING_DIRECTORY/*.fastq.gz)
+do
+      fastqc $FILE -o $OUTPUT
+done ;
+
+
+conda deactivate fastqc
+conda activate multiqc
+
+# Run multiqc for quality summary
+
+multiqc $OUTPUT
+
